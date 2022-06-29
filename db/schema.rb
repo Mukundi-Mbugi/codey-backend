c78bbda50253af3d3adc8960abb74266d6fb6a35
Elijah Mbugi <mukundielijah@gmail.com>
2022-06-29 14:55:46 +0300

    modify migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2) do
     t.integer "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "likes"
   end
 
 end
